edf63dd32d45318dfcd65e63674d880e8a20483e
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-24 19:06:02 -0300

    updating structure

--- src/components/TextSessionTitle.tsx
+++ src/components/TextSessionTitle.tsx
@@ -1,13 +1,14 @@
+import Text from "./Text";
+import Title from "./Title";
+
 const TextSessionTitle = () => {
   return (
-    <div className="max-w-xs grid gap-5 text-center mx-auto pt-24 px-5 pb-28">
-      <h1 className="text-2xl text-zinc-100 font-bold">
-        3D elements with React
-      </h1>
-      <p className="text-zinc-100/70 text-sm">
+    <div className="max-w-xs grid gap-5 text-center mx-auto pt-24 px-5 pb-20">
+      <Title>3D elements with React</Title>
+      <Text>
         Transform your website into a visually impactful experience with amazing
         3D effects
-      </p>
+      </Text>
     </div>
   );
 };
