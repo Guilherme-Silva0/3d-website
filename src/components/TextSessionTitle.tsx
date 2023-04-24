6213498660ec23594f32aa0209bd09def03042dc
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-24 14:07:27 -0300

    creating TextSessionTitle component

---
+++ src/components/TextSessionTitle.tsx
@@ -0,0 +1,15 @@
+const TextSessionTitle = () => {
+  return (
+    <div className="max-w-xs grid gap-5 text-center mx-auto pt-24 px-5 pb-28">
+      <h1 className="text-2xl text-zinc-100 font-bold">
+        3D elements with React
+      </h1>
+      <p className="text-zinc-100/70 text-sm">
+        Transform your website into a visually impactful experience with amazing
+        3D effects
+      </p>
+    </div>
+  );
+};
+
+export default TextSessionTitle;
